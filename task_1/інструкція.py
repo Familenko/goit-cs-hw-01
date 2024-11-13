1629993751a51bff9c19555fabeb476d5fd1dd63
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2024-11-13 15:55:31 +0200

    task 1 solved

---
+++ task_1/інструкція.py
@@ -0,0 +1,12 @@
+запускаємо в терміналі
+	# шлях
+	cd /Users/aleksejkitajskij/Desktop/nasm_folder
+
+	# команда для компіляції
+	./nasm -f bin -o my_code.com my_code.asm
+
+далі в DOSBox
+
+	mount c /Users/aleksejkitajskij/Desktop/nasm_folder
+	c:
+	my_code.com
